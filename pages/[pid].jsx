96f8ba0de9b3a2165e8a91cf2c8bb45c85034918
omkargarde <42486951+omkargarde@users.noreply.github.com>
2023-08-14 14:54:21 +0530

    added pid

---
+++ pages/[pid].jsx
@@ -0,0 +1,27 @@
+import fs from "fs/promises";
+import path from "path";
+export default function ProductDetailPage(props) {
+  const { product } = props;
+  return (
+    <>
+      <h1>{product.title}</h1>
+      <p>{product.description}</p>
+    </>
+  );
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const productId = params.pid;
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  const product = data.products.find((product) => {
+    product.id === productId;
+  });
+  return {
+    props: {
+      product: product,
+    },
+  };
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,29 @@
+import fs from "fs/promises";
 import Link from "next/link";
-
-export default function HomePage() {
+import path from "path";
+export default function HomePage(props) {
+  const products = props.products;
   return (
     <div>
       <h1>HomePage</h1>
-      
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            <Link href={`${product.id}`}>{product.title}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
+}
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+  return {
+    props: {
+      products: data.products,
+    },
+    revalidate: 120,
+  };
 }
